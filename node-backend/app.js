366ac9ef4b336eb4a70f2d3413bb7ecfe65ebaf8
er-devarun <137439924+er-devarun@users.noreply.github.com>
2024-02-03 19:27:42 +0530

    Update app.js

--- node-backend/app.js
+++ node-backend/app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   setTimeout(()=>{
     res.send("Hello world! Your Server is live now");
-  }, 10000);
+  }, 7000);
 });
 
 app.get('/item', (req, res) => {
