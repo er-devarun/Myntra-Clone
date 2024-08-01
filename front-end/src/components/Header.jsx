9246ee30e5f0520b4439bf14868129ae6d0508dd
Arun Dev <er.devarun@gmail.com>
2024-08-02 01:29:07 +0530

    update css

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -26,37 +26,39 @@ function Header(){
 
     return(
         <header>
-            <div className="logo-container">
-                <Link to="/"><img className="myntra-home" src="/images/myntra_logo.webp" alt="myntra_home"/></Link>
-            </div>
-            <nav className="nav-bar">
-                <a href="#">MEN</a>
-                <a href="#">WOMEN</a>
-                <a href="#">KIDS</a>
-                <a href="#">HOME & LIVING</a>
-                <a href="#">BEAUTY</a>
-                <a href="#">STUDIO<sup>NEW</sup></a>
-            </nav>
-            <div className="search-bar">
-                <IoSearch className="search-icon"/>
-                <input className="search-input" placeholder="Search for products, brands and more" value={serchItem} onChange={(event) => handleSearchBox(event)} onKeyDown={(event) => handleSearchInput(event)}/>
-            </div>
-            <div className="action-bar">
-                <div className="action-container">
-                    <IoPerson className="action-icon"/>
-                    <span className="action-name">Profile</span>
+            <div className="header-container">
+                <div className="logo-container">
+                    <Link to="/"><img className="myntra-home" src="/images/myntra_logo.webp" alt="myntra_home"/></Link>
                 </div>
-
-                <div className="action-container">
-                    <FaHeart className="action-icon"/>
-                    <span className="action-name">Wishlist</span>
+                <nav className="nav-bar">
+                    <a href="#">MEN</a>
+                    <a href="#">WOMEN</a>
+                    <a href="#">KIDS</a>
+                    <a href="#">HOME & LIVING</a>
+                    <a href="#">BEAUTY</a>
+                    <a href="#">STUDIO<sup>NEW</sup></a>
+                </nav>
+                <div className="search-bar">
+                    <IoSearch className="search-icon"/>
+                    <input className="search-input" placeholder="Search for products, brands and more" value={serchItem} onChange={(event) => handleSearchBox(event)} onKeyDown={(event) => handleSearchInput(event)}/>
                 </div>
+                <div className="action-bar">
+                    <div className="action-container">
+                        <IoPerson className="action-icon"/>
+                        <span className="action-name">Profile</span>
+                    </div>
 
-                <Link className="action-container" to="/bag">
-                    <IoBagHandle className="action-icon"/>
-                    <span className="action-name">Bag</span>
-                    {bag.length > 0 && <span className="bag-item-count">{bag.length}</span>}
-                </Link>
+                    <div className="action-container">
+                        <FaHeart className="action-icon"/>
+                        <span className="action-name">Wishlist</span>
+                    </div>
+
+                    <Link className="action-container" to="/bag">
+                        <IoBagHandle className="action-icon"/>
+                        <span className="action-name">Bag</span>
+                        {bag.length > 0 && <span className="bag-item-count">{bag.length}</span>}
+                    </Link>
+                </div>
             </div>
         </header>
     );
